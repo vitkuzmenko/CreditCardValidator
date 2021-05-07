97cde6f3cbd0bfbe537a5df0df3651aaa9336cc8
vitkuzmenko <kuzmenko.v.u@gmail.com>
2021-05-07 10:05:42 +0300

    UPdated for swift features

--- Sources/CreditCardValidator/CreditCardValidator.swift
+++ Sources/CreditCardValidator/CreditCardValidator.swift
@@ -1,125 +1,91 @@
-//
-//  CreditCardValidator.swift
-//
-//  Created by Vitaliy Kuzmenko on 02/06/15.
-//  Copyright (c) 2015. All rights reserved.
-//
-
 import Foundation
 
-public class CreditCardValidator {
-    public lazy var types: [CreditCardValidationType] = {
-        var types = [CreditCardValidationType]()
-        for object in CreditCardValidator.types {
-            types.append(CreditCardValidationType(dict: object))
-        }
-        return types
-        }()
+public enum CreditCardType: String {
+    case amex = "^3[47][0-9]{5,}$"
+    case visa = "^4[0-9]{6,}$"
+    case masterCard = "^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$"
+    case maestro = "^(?:5[0678]\\d\\d|6304|6390|67\\d\\d)\\d{8,15}$"
+    case dinersClub = "^3(?:0[0-5]|[68][0-9])[0-9]{4,}$"
+    case jcb = "^(?:2131|1800|35[0-9]{3})[0-9]{3,}$"
+    case discover = "^6(?:011|5[0-9]{2})[0-9]{3,}$"
+    case unionPay = "^62[0-5]\\d{13,16}$"
+    case mir = "^2[0-9]{6,}$"
+}
+
+public struct CreditCardValidator {
     
-    public init() { }
+    /// Available credit card types
+    private let types: [CreditCardType] = [
+        .amex,
+        .visa,
+        .masterCard,
+        .maestro,
+        .dinersClub,
+        .jcb,
+        .discover,
+        .unionPay,
+        .mir
+    ]
     
-    /**
-    Get card type from string
+    private let string: String
     
-    - parameter string: card number string
-    
-    - returns: CreditCardValidationType structure
-    */
-    public func type(from string: String) -> CreditCardValidationType? {
-        for type in types {
-            let predicate = NSPredicate(format: "SELF MATCHES %@", type.regex)
-            let numbersString = self.onlyNumbers(string: string)
-            if predicate.evaluate(with: numbersString) {
-                return type
-            }
-        }
-        return nil
+    /// Create validation value
+    /// - Parameter string: credit card number
+    public init(_ string: String) {
+        self.string = string.numbers
     }
     
-    /**
-    Validate card number
-    
-    - parameter string: card number string
-    
-    - returns: true or false
-    */
-    public func validate(string: String) -> Bool {
-        let numbers = self.onlyNumbers(string: string)
-        if numbers.count < 9 {
-            return false
+    /// Get card type
+    /// Card number validation is not perfroms here
+    public var type: CreditCardType? {
+        types.first { type in
+            NSPredicate(format: "SELF MATCHES %@", type.rawValue)
+                .evaluate(
+                    with: string.numbers
+                )
         }
-        
-        var reversedString = ""
-        let range: Range<String.Index> = numbers.startIndex..<numbers.endIndex
-        
-        numbers.enumerateSubstrings(in: range, options: [.reverse, .byComposedCharacterSequences]) { (substring, substringRange, enclosingRange, stop) -> () in
-            reversedString += substring!
-        }
-        
-        var oddSum = 0, evenSum = 0
-        
-        for (i, s) in reversedString.enumerated() {
-            
-            let digit = Int(String(s))!
-            
-            if i % 2 == 0 {
-                evenSum += digit
-            } else {
-                oddSum += digit / 5 + (2 * digit) % 10
-            }
-        }
-        return (oddSum + evenSum) % 10 == 0
     }
     
-    /**
-    Validate card number string for type
-    
-    - parameter string: card number string
-    - parameter type:   CreditCardValidationType structure
-    
-    - returns: true or false
-    */
-    public func validate(string: String, forType type: CreditCardValidationType) -> Bool {
-        return self.type(from: string) == type
+    /// Calculation structure
+    private struct Calculation {
+        let odd, even: Int
+        func result() -> Bool {
+            (odd + even) % 10 == 0
+        }
     }
     
-    public func onlyNumbers(string: String) -> String {
+    /// Validate credit card number
+    public var isValid: Bool {
+        string.count >= 9 && string
+            .reversed()
+            .compactMap({ Int(String($0)) })
+            .enumerated()
+            .reduce(Calculation(odd: 0, even: 0), { value, iterator in
+                return .init(
+                    odd: iterator.offset % 2 != 0 ? value.odd + (iterator.element / 5 + (2 * iterator.element) % 10) : value.odd,
+                    even: iterator.offset % 2 == 0 ? value.even + iterator.element : value.even
+                )
+            })
+            .result()
+    }
+    
+    /// Validate card number string for type
+    /// - Parameters:
+    ///   - string: card number string
+    ///   - type: credit card type
+    /// - Returns: bool value
+    public func isValid(for type: CreditCardType) -> Bool {
+        isValid && self.type == type
+    }
+    
+}
+
+fileprivate extension String {
+ 
+    var numbers: String {
         let set = CharacterSet.decimalDigits.inverted
-        let numbers = string.components(separatedBy: set)
+        let numbers = components(separatedBy: set)
         return numbers.joined(separator: "")
     }
-    
-    // MARK: - Loading data
-    
-    private static let types = [
-        [
-            "name": "Amex",
-            "regex": "^3[47][0-9]{5,}$"
-        ], [
-            "name": "Visa",
-            "regex": "^4\\d{0,}$"
-        ], [
-            "name": "MasterCard",
-            "regex": "^5[1-5]\\d{0,14}$"
-        ], [
-            "name": "Maestro",
-            "regex": "^(?:5[0678]\\d\\d|6304|6390|67\\d\\d)\\d{8,15}$"
-        ], [
-            "name": "Diners Club",
-            "regex": "^3(?:0[0-5]|[68][0-9])[0-9]{4,}$"
-        ], [
-            "name": "JCB",
-            "regex": "^(?:2131|1800|35[0-9]{3})[0-9]{3,}$"
-        ], [
-            "name": "Discover",
-            "regex": "^6(?:011|5[0-9]{2})[0-9]{3,}$"
-        ], [
-            "name": "UnionPay",
-            "regex": "^62[0-5]\\d{13,16}$"
-        ], [
-            "name": "Mir",
-            "regex": "^22[0-9]{1,14}$"
-        ]
-    ]
     
 }
